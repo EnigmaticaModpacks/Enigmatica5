63d49d37ce2b582a20f0b1c92775371690603dc7
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-03-12 22:10:30 +0100

    Update settings.ps1

--- settings.ps1
+++ settings.ps1
@@ -62,9 +62,10 @@ $GAME_VERSIONS = @(7722) # 7675 is 1.15.1, 7722 is 1.15.2
 $CLIENT_CHANGELOG_TYPE = "markdown"
 
 $CLIENT_CHANGELOG = "### Enigmatica 5 v$MODPACK_VERSION <br /> " +
-					"*Using Forge-1.15.2-31.1.17* <br /> " +
-					"[Changelog](https://github.com/NillerMedDild/Enigmatica5/blob/master/CHANGELOG.md) <br /> " +
-					"[Mod Updates](https://github.com/NillerMedDild/Enigmatica5/blob/master/CHANGELOG-MODS.md) "
+					"*Using Forge-1.15.2-31.1.17* <br /> <br /> " +
+					"[Mod Updates](https://github.com/NillerMedDild/Enigmatica5/blob/master/changelogs/CHANGELOG_MODS_$MODPACK_VERSION.txt) <br /> " +
+					"[Changelog](https://github.com/NillerMedDild/Enigmatica5/blob/master/CHANGELOG.md) "
+
 
 # Can be "alpha", "beta" or "release"
 $CLIENT_RELEASE_TYPE = "beta"
