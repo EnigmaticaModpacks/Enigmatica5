83a8aeeac2851314dea9b8647c0359b22adf1a14
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-03-22 21:45:44 +0100

    Removed references to bluepower

--- kubejs/data/enigmatica/kubejs/tags/constants.js
+++ kubejs/data/enigmatica/kubejs/tags/constants.js
@@ -12,24 +12,14 @@ var oreUraninite = [
 
 var stoneBasalt = [
     // 'quark:basalt', 
-    'bluepower:basalt', 
-    'bluepower:basalt_brick', 
     // 'quark:chiseled_basalt_bricks', 
     // 'quark:basalt_pavement', 
     // 'quark:basalt_pillar', 
     // 'quark:polished_basalt', 
     // 'quark:basalt_bricks', 
-    'bluepower:basalt_brick_small',
-    'bluepower:fancy_basalt', 
-    'bluepower:basalt_tile', 
-    'bluepower:basalt_paver',
-    'bluepower:tiles', 
-    'bluepower:basaltbrick_cracked', 
-    'bluepower:cracked_basalt_lava'
 ];
 
 var cobblestoneBasalt = [
-    'bluepower:basalt_cobble'
 ];
 
 var materials = global.materials;
