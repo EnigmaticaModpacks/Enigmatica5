3f3477fa437f2e29b095dcd946d17c3042f801d5
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-04-14 00:14:51 +0200

    Packmode loading a bit earlier

--- kubejs/data/enigmatica/kubejs/packmode.js
+++ kubejs/data/enigmatica/kubejs/packmode.js
@@ -1,4 +1,4 @@
-events.listen('server.datapacks.first', function(event) {
+events.listen('scripts_loaded', function(event) {
     var config = json.read("config/packmode.json");
     if(!config || !config.packmode) {
         config = { packmode: "normal" };
