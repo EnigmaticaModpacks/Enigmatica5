705eae73977f902c267382fbfa062a42f73bdff1
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-04-14 00:22:01 +0200

    Update packmode.js

--- kubejs/data/enigmatica/kubejs/packmode.js
+++ kubejs/data/enigmatica/kubejs/packmode.js
@@ -1,8 +1,11 @@
-events.listen('scripts_loaded', function(event) {
-    var config = json.read("config/packmode.json");
-    if(!config || !config.packmode) {
-        config = { packmode: "normal" };
-      json.write("config/packmode.json", config);
-    }
-    global.packmode = config.packmode;
-  })
+events.listen('scripts_loaded', function (event) {
+	var config = json.read("config/packmode.json");
+	if (!config || !config.packmode) {
+		config = {
+			packmode: "normal",
+			message: "Valid modes are \"normal\" and \"expert\"."
+		};
+		json.write("config/packmode.json", config);
+	}
+	global.packmode = config.packmode;
+})
