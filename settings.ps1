4b389933aeafe96a4ab834fa87ba105ff67d0f2a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-04-23 22:01:44 +0200

    Version bump

--- settings.ps1
+++ settings.ps1
@@ -40,10 +40,10 @@ $CURSEFORGE_PROJECT_ID = 357692
 # =====================================================================//
 
 # Default: "1.0.0"
-$MODPACK_VERSION = "0.4.7"
+$MODPACK_VERSION = "0.4.8"
 
 # Only used by the Changelog Generator
-$LAST_MODPACK_VERSION = "0.4.6"
+$LAST_MODPACK_VERSION = "0.4.7"
 
 $FORGE_VERSION=31.1.46
 
