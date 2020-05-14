33561915733fa4998b28e2626ab1d3732f4a7d8d
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-05-14 22:44:53 +0200

    Figured out how to move recipes from CrT

---
+++ kubejs/data/enigmatica/kubejs/functions.js
@@ -0,0 +1,3 @@
+function recipe(result, pattern, key) {
+    return { result: result, pattern: pattern, key: key };
+}
--- kubejs/data/enigmatica/kubejs/recipes/base/shaped.js
+++ kubejs/data/enigmatica/kubejs/recipes/base/shaped.js
@@ -1,7 +1,13 @@
 events.listen('server.datapack.recipes', function (event) {
-    event.shaped('pneumaticcraft:speed_upgrade', ['LXL', 'XCX', 'LXL'], {
-        L: '#pneumaticcraft:upgrade_components',
-        X: 'minecraft:sugar',
-        C: 'pneumaticcraft:lubricant_bucket',
+    var recipes = [
+        recipe('pneumaticcraft:speed_upgrade', ['LXL', 'XCX', 'LXL'], {
+            L: '#pneumaticcraft:upgrade_components',
+            X: 'minecraft:sugar',
+            C: 'pneumaticcraft:lubricant_bucket',
+        }),
+    ];
+
+    recipes.forEach(function (recipe) {
+        event.shaped(recipe.result, recipe.pattern, recipe.key);
     });
 });
