663651757c19f0f9eacbbd2a9288b5844ae1460a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-05-14 22:15:30 +0200

    Fixes #307

--- kubejs/data/enigmatica/kubejs/recipes/base/shaped.js
+++ kubejs/data/enigmatica/kubejs/recipes/base/shaped.js
@@ -1,1 +1,7 @@
-events.listen('server.datapack.recipes', function (event) {})
+events.listen('server.datapack.recipes', function (event) {
+    event.shaped('pneumaticcraft:speed_upgrade', ['LXL', 'XCX', 'LXL'], {
+        L: '#pneumaticcraft:upgrade_components',
+        X: 'minecraft:sugar',
+        C: 'pneumaticcraft:lubricant_bucket',
+    });
+});
