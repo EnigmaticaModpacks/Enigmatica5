3bc9dcf7899757a07c3f9d912239304d764a6cea
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-05-24 00:28:20 +0200

    Let's see if this fixes it then #315

--- kubejs/data/enigmatica/kubejs/base/tags/items/forge/dusts.js
+++ kubejs/data/enigmatica/kubejs/base/tags/items/forge/dusts.js
@@ -1,10 +1,10 @@
 events.listen('item.tags', function (event) {
-    event.get('forge:dusts/lapis').add('mekanism:dust_lapis_lazuli')
-    event.get('forge:dusts/copper').add('occultism:copper_dust')
-    event.get('forge:dusts/iron').add('occultism:iron_dust')
-    event.get('forge:dusts/silver').add('occultism:silver_dust')
-    event.get('forge:dusts/gold').add('occultism:gold_dust')
-    event
-        .get('forge:dusts')
-        .add(['occultism:copper_dust', 'occultism:iron_dust', 'occultism:silver_dust', 'occultism:gold_dust'])
-})
+    event.get('forge:dusts/lapis').add('mekanism:dust_lapis_lazuli');
+    // event.get('forge:dusts/copper').add('occultism:copper_dust')
+    // event.get('forge:dusts/iron').add('occultism:iron_dust')
+    // event.get('forge:dusts/silver').add('occultism:silver_dust')
+    // event.get('forge:dusts/gold').add('occultism:gold_dust')
+    // event
+    //     .get('forge:dusts')
+    //     .add(['occultism:copper_dust', 'occultism:iron_dust', 'occultism:silver_dust', 'occultism:gold_dust'])
+});
