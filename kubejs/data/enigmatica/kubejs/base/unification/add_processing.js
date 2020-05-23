a545f7a05e37bf8c132a731838b75a7683f8b08b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-05-24 01:17:34 +0200

    Scrapped most of the attempt at KJS unification

--- kubejs/data/enigmatica/kubejs/base/unification/add_processing.js
+++ kubejs/data/enigmatica/kubejs/base/unification/add_processing.js
@@ -1,79 +1,79 @@
-materialsToUnify.forEach(function (material) {
-    // enigmatica_ore_deposit_processing(material);
-    // occultism_ore_inot_crushing(material);
-});
+// materialsToUnify.forEach(function (material) {
+//     // enigmatica_ore_deposit_processing(material);
+//     // occultism_ore_inot_crushing(material);
+// });
 
-function enigmatica_ore_deposit_processing(material) {
-    // Doesn't work, WTB documentation
+// function enigmatica_ore_deposit_processing(material) {
+//     // Doesn't work, WTB documentation
 
-    events.listen('recipes', function (event) {
-        console.log('Starting enigmatica_ore_deposit_processing5 with ' + material);
-        var ore_deposit_ingredient = ingredient.of('#forge:ore_deposits/' + material);
-        var dust_ingredient = ingredient.of('#forge:dusts/' + material);
-        var ore_deposit = ore_deposit_ingredient.getFirst();
-        var dust = dust_ingredient.getFirst();
-        var byproduct = 'minecraft:dirt';
-        var byproductChance = 0.5;
+//     events.listen('recipes', function (event) {
+//         console.log('Starting enigmatica_ore_deposit_processing with ' + material);
+//         var ore_deposit_ingredient = ingredient.of('#forge:ore_deposits/' + material);
+//         var dust_ingredient = ingredient.of('#forge:dusts/' + material);
+//         var ore_deposit = ore_deposit_ingredient.getFirst();
+//         var dust = dust_ingredient.getFirst();
+//         var byproduct = 'minecraft:dirt';
+//         var byproductChance = 0.5;
 
-        if (!ore_deposit) {
-            console.log('Ore Deposit Ingredient with name #forge:ore_deposits/' + material + ' was null.');
-            return;
-        }
+//         if (!ore_deposit) {
+//             console.log('Ore Deposit Ingredient with name #forge:ore_deposits/' + material + ' was null.');
+//             return;
+//         }
 
-        if (!dust) {
-            console.log('Dust Ingredient with name #forge:dusts/' + material + ' was null.');
-            return;
-        }
+//         if (!dust) {
+//             console.log('Dust Ingredient with name #forge:dusts/' + material + ' was null.');
+//             return;
+//         }
 
-        var xp = 0.5;
-        var processingTime = 100;
+//         var xp = 0.5;
+//         var processingTime = 100;
 
-        event.recipes.minecraft.smelting({
-            ingredient: {
-                item: ore_deposit
-            },
-            result: dust,
-            experience: xp,
-            cookingtime: processingTime
-        });
-        event.recipes.minecraft.blasting({
-            ingredient: {
-                item: ore_deposit
-            },
-            result: dust,
-            experience: xp,
-            cookingtime: processingTime / 2
-        });
+//         event.recipes.minecraft.smelting({
+//             ingredient: {
+//                 item: ore_deposit
+//             },
+//             result: dust,
+//             experience: xp,
+//             cookingtime: processingTime
+//         });
+//         event.recipes.minecraft.blasting({
+//             ingredient: {
+//                 item: ore_deposit
+//             },
+//             result: dust,
+//             experience: xp,
+//             cookingtime: processingTime / 2
+//         });
 
-        event.recipes.mekanism.enriching({
-            input: {
-                ingredient: {
-                    item: ore_deposit
-                }
-            },
-            output: {
-                item: dust
-            }
-        });
+//         event.recipes.mekanism.enriching({
+//             input: {
+//                 ingredient: {
+//                     item: ore_deposit
+//                 }
+//             },
+//             output: {
+//                 item: dust
+//             }
+//         });
 
-        event.recipes.silents_mechanisms.crushing({
-            process_time: 200,
-            ingredient: {
-                item: ore_deposit
-            },
-            results: [
-                {
-                    item: dust
-                },
-                {
-                    item: byproduct,
-                    chance: byproductChance
-                }
-            ]
-        });
-    });
-}
+//         event.recipes.silents_mechanisms.crushing({
+//             process_time: 200,
+//             ingredient: {
+//                 item: ore_deposit
+//             },
+//             results: [
+//                 {
+//                     item: dust
+//                 },
+//                 {
+//                     item: byproduct,
+//                     chance: byproductChance
+//                 }
+//             ]
+//         });
+//     });
+// }
 
-function occultism_ore_inot_crushing(material) {
-    events.listen('recipes', function (event) {});
-}
+// function occultism_ore_inot_crushing(material) {
+//     events.listen('recipes', function (event) {});
+// }
--- kubejs/data/enigmatica/kubejs/base/unification/early_removals.js
+++
@@ -1,8 +0,0 @@
-events.listen('recipes', function (event) {
-    event.remove({ output: 'occultism:iron_dust' });
-    event.remove({ output: 'occultism:gold_dust' });
-    event.remove({ output: 'occultism:silver_dust' });
-    event.remove({ output: 'occultism:copper_dust' });
-
-    event.remove({ type: 'mekanism:combining' }); // Remove all combining recipes
-});
