9c9ee930d11659e9cb0aa50282760ceee65f746a
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-05-29 20:52:43 +0200

    Added energized_Steel to it's own tag

--- kubejs/data/enigmatica/kubejs/base/tags/items/forge/ingots.js
+++ kubejs/data/enigmatica/kubejs/base/tags/items/forge/ingots.js
@@ -1,6 +1,6 @@
 events.listen('item.tags', function (event) {
-    event.get('forge:ingots').add('powah:uraninite')
-    event.get('forge:ingots/uraninite').add('powah:uraninite')
-
-    event.get('forge:ingots/radioactive').add('#forge:ingots/uraninite').add('#forge:ingots/uranium')
-})
+    event.get('forge:ingots').add('powah:uraninite');
+    event.get('forge:ingots/uraninite').add('powah:uraninite');
+    event.get('forge:ingots/energized_steel').add('powah:steel_energized');
+    event.get('forge:ingots/radioactive').add('#forge:ingots/uraninite').add('#forge:ingots/uranium');
+});
