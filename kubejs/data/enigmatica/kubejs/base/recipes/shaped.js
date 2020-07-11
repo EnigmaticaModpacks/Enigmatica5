7769d13f819241300e4e93db7d559ba77644a69d
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-07-11 21:45:58 +0200

    Migrated the last scripts to KJS!

--- kubejs/data/enigmatica/kubejs/base/recipes/shaped.js
+++ kubejs/data/enigmatica/kubejs/base/recipes/shaped.js
@@ -12,48 +12,76 @@ events.listen('recipes', function (event) {
         }),
         shapedRecipe('minecraft:stick 16', ['L', 'L'], {
             A: '#minecraft:logs'
-        })
+        }),
 
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     A: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // }),
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     A: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // }),
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     L: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // }),
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     L: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // }),
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     L: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // }),
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     L: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // }),
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     L: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // }),
-        // shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
-        //     L: '#minecraft:logs',
-        //     B: '',
-        //     C: ''
-        // })
+        shapedRecipe('minecraft:hopper', ['ABA', 'ABA', ' A '], {
+            A: '#forge:ingots/iron',
+            B: '#minecraft:logs'
+        }),
+        shapedRecipe('refinedstorageaddons:wireless_crafting_grid', ['ABA', 'ACA', 'ADA'], {
+            A: '#refinedstorage:quartz_enriched_iron',
+            B: 'refinedstorage:range_upgrade',
+            C: 'refinedstorage:wireless_grid',
+            D: 'minecraft:crafting_table'
+        }),
+        shapedRecipe('powah:reactor_starter', ['ABA', 'BCB', 'ABA'], {
+            A: '#forge:ingots/radioactive',
+            B: 'powah:capacitor_basic_tiny',
+            C: 'powah:dielectric_casing'
+        }),
+        shapedRecipe('powah:reactor_basic', ['ABA', 'BCB', 'ABA'], {
+            A: '#forge:ingots/radioactive',
+            B: 'powah:capacitor_basic_large',
+            C: 'powah:dielectric_casing'
+        }),
+        shapedRecipe('powah:reactor_hardened', ['ABA', 'BCB', 'ABA'], {
+            A: '#forge:ingots/radioactive',
+            B: 'powah:capacitor_hardened',
+            C: 'powah:dielectric_casing'
+        }),
+        shapedRecipe('powah:reactor_blazing', ['ABA', 'BCB', 'ABA'], {
+            A: '#forge:ingots/radioactive',
+            B: 'powah:capacitor_blazing',
+            C: 'powah:dielectric_casing'
+        }),
+        shapedRecipe('powah:reactor_niotic', ['ABA', 'BCB', 'ABA'], {
+            A: '#forge:ingots/radioactive',
+            B: 'powah:capacitor_niotic',
+            C: 'powah:dielectric_casing'
+        }),
+        shapedRecipe('powah:reactor_spirited', ['ABA', 'BCB', 'ABA'], {
+            A: '#forge:ingots/radioactive',
+            B: 'powah:capacitor_spirited',
+            C: 'powah:dielectric_casing'
+        }),
+        shapedRecipe('powah:reactor_nitro', ['ABA', 'BCB', 'ABA'], {
+            A: '#forge:ingots/radioactive',
+            B: 'powah:capacitor_nitro',
+            C: 'powah:dielectric_casing'
+        }),
+        shapedRecipe('simplefarming:fish_and_chips', [' A ', 'BCB', ' A '], {
+            A: '#forge:crops/potato',
+            B: '#forge:grain',
+            C: 'aquaculture:fish_fillet_cooked'
+        }),
+        shapedRecipe('minecraft:furnace', ['LLL', 'L L', 'LLL'], {
+            L: '#forge:stone'
+        }),
+        shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
+            A: '#minecraft:logs',
+            B: '',
+            C: ''
+        }),
+        shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
+            A: '#minecraft:logs',
+            B: '',
+            C: ''
+        }),
+        shapedRecipe('', ['LLL', 'LLL', 'LLL'], {
+            L: '#minecraft:logs',
+            B: '',
+            C: ''
+        })
     ];
 
     recipes.forEach(function (recipe) {
