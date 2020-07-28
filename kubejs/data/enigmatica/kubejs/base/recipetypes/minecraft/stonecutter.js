be0cc09d8145519da71a728429d53011102f3424
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-07-28 16:54:15 +0200

    Fixed stonecutter compat

--- kubejs/data/enigmatica/kubejs/base/recipetypes/minecraft/stonecutter.js
+++ kubejs/data/enigmatica/kubejs/base/recipetypes/minecraft/stonecutter.js
@@ -1,26 +1,16 @@
 events.listen('recipes', function (event) {
     vanillaWoodTypes.forEach(function (woodType) {
-        var log = 'minecraft:' + woodType + '_logs';
-        var wood_floor = 'carpetstairsmod:' + woodType + '_wood_floor';
-        var planks = 'minecraft:' + woodType + '_planks';
-
-        // Create Stonecutter recipes for both.
-        event.recipes.minecraft.stonecutting({
-            ingredient: {
-                tag: log
-            },
-            result: wood_floor,
-            count: 24
-        });
-        event.recipes.minecraft.stonecutting({
-            ingredient: {
-                item: planks
-            },
-            result: wood_floor,
-            count: 6
-        });
+        event.recipes.minecraft.stonecutting(
+            item.of('carpetstairsmod:' + woodType + '_wood_floor', 6),
+            'minecraft:' + woodType + '_planks'
+        );
+        event.recipes.minecraft.stonecutting(
+            item.of('carpetstairsmod:' + woodType + '_wood_floor', 24),
+            '#minecraft:' + woodType + '_logs'
+        );
     });
 
+    // Conversion between different storage_blocks of the same material
     materialsToUnify.forEach(function (material) {
         var storage_block_tag = ingredient.of('#forge:storage_blocks/' + material);
         if (storage_block_tag.stacks.size() > 1) {
