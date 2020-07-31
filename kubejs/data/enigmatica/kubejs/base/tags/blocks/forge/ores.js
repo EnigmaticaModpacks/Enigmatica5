3a85d11e06b62424aa34cfa36f06d8895c523d39
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-07-31 22:12:38 +0200

    Fixes #386

--- kubejs/data/enigmatica/kubejs/base/tags/blocks/forge/ores.js
+++ kubejs/data/enigmatica/kubejs/base/tags/blocks/forge/ores.js
@@ -3,4 +3,21 @@ events.listen('block.tags', function (event) {
     event.get('forge:ores/dimensional').add(oreDimensional);
     event.get('forge:ores').add('create:zinc_ore');
     event.get('forge:ores').add('occultism:iesnium_ore');
+
+    var unusedOres = [
+        'create:copper_ore',
+        'mekanism:copper_ore',
+        'occultism:copper_ore',
+        'occultism:silver_ore',
+        'tmechworks:aluminum_ore',
+        'tmechworks:copper_ore'
+    ];
+
+    event.get('forge:ores').remove(unusedOres);
+    event
+        .get('forge:ores/copper')
+        .remove(['create:copper_ore', 'mekanism:copper_ore', 'occultism:copper_ore', 'tmechworks:copper_ore']);
+
+    event.get('forge:ores/silver').remove('occultism:silver_ore');
+    event.get('forge:ores/aluminum').remove('tmechworks:aluminum_ore');
 });
