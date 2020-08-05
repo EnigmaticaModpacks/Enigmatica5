c6a1ea3c60d675774ba765e55f90b41ae6fff46c
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-08-05 22:45:25 +0200

    Unregistered custom ore blocks

--- kubejs/startup/factory/blocks.js
+++ kubejs/startup/factory/blocks.js
@@ -13,17 +13,17 @@ events.listen('block.registry', function (event) {
         });
     });
 
-    oreStoneVariants.forEach(function (stoneVariant) {
-        oreVariants.forEach(function (oreVariant) {
-            var registryName = stoneVariant + '_' + oreVariant.name + '_ore';
-            var block = oreVariant.blockParts[0];
-            event
-                .create(registryName)
-                .texture('kubejs:block/' + registryName)
-                .material(block.material)
-                .harvestTool(block.harvestTool, block.harvestLevel)
-                .hardness(block.hardness)
-                .resistance(block.resistance);
-        });
-    });
+    // oreStoneVariants.forEach(function (stoneVariant) {
+    //     oreVariants.forEach(function (oreVariant) {
+    //         var registryName = stoneVariant + '_' + oreVariant.name + '_ore';
+    //         var block = oreVariant.blockParts[0];
+    //         event
+    //             .create(registryName)
+    //             .texture('kubejs:block/' + registryName)
+    //             .material(block.material)
+    //             .harvestTool(block.harvestTool, block.harvestLevel)
+    //             .hardness(block.hardness)
+    //             .resistance(block.resistance);
+    //     });
+    // });
 });
